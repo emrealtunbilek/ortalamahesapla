0bd4da9b3b20597173311cf3e38665ec34101098
Emre Altunbilek <emrealtunbilek@gmail.com>
2017-12-14 03:14:28 +0300

    2. ders tasarım genel hatlarıyla bitti

--- app/src/main/java/com/emrealtunbilek/ortalamahesapla/MainActivity.kt
+++ app/src/main/java/com/emrealtunbilek/ortalamahesapla/MainActivity.kt
@@ -2,11 +2,17 @@ package com.emrealtunbilek.ortalamahesapla
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun ortalamaHesapla(view: View){
+
     }
 }
