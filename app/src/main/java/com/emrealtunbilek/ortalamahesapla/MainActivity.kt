1c72b3eeb9b024f8678b8f2f9a43c1d4b89bf825
Emre Altunbilek <emrealtunbilek@gmail.com>
2017-12-15 21:20:29 +0300

    5 Layout silme, view gizleme ve değerleri sıfırlama

--- app/src/main/java/com/emrealtunbilek/ortalamahesapla/MainActivity.kt
+++ app/src/main/java/com/emrealtunbilek/ortalamahesapla/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Spinner
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.yeni_ders_layout.*
 import kotlinx.android.synthetic.main.yeni_ders_layout.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,38 +17,68 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        if(rootLayout.childCount == 0){
+            btnOrtalamaHesapla.visibility = View.INVISIBLE
+        }else btnOrtalamaHesapla.visibility = View.VISIBLE
+
+
         btnDersEkle.setOnClickListener {
 
-            var inflater = LayoutInflater.from(this)
-           /* var inflater2 = layoutInflater
-            var inflater3=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            inflater3.inflate()*/
+            if(!etDersAd.text.isNullOrEmpty()){
+                var inflater = LayoutInflater.from(this)
+                /* var inflater2 = layoutInflater
+                 var inflater3=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                 inflater3.inflate()*/
 
-            var yeniDersView = inflater.inflate(R.layout.yeni_ders_layout, null)
+                var yeniDersView = inflater.inflate(R.layout.yeni_ders_layout, null)
 
-            //statik alandan kullanıcının girdiği değerleri alalım
-            var dersAdi = etDersAd.text.toString()
-            var dersKredi = spnDersKredi.selectedItem.toString()
-            var dersHarf = spnDersNot.selectedItem.toString()
-
-
-            //dinamik oluşturulacak layoutta bulunan view öğelerine bu değerleri atayalım
-            //böylece yeni layoutumuz kullanıcının girmiş oldugu değerler ile oluşturulacaktır.
-            yeniDersView.etYeniDersAd.setText(dersAdi)
-            yeniDersView.spnYeniDersKredi.setSelection(spinnerDegerinIndexiniBul(spnDersKredi, dersKredi))
-            yeniDersView.spnYeniDersNot.setSelection(spinnerDegerinIndexiniBul(spnDersNot, dersHarf))
+                //statik alandan kullanıcının girdiği değerleri alalım
+                var dersAdi = etDersAd.text.toString()
+                var dersKredi = spnDersKredi.selectedItem.toString()
+                var dersHarf = spnDersNot.selectedItem.toString()
 
 
+                //dinamik oluşturulacak layoutta bulunan view öğelerine bu değerleri atayalım
+                //böylece yeni layoutumuz kullanıcının girmiş oldugu değerler ile oluşturulacaktır.
+                yeniDersView.etYeniDersAd.setText(dersAdi)
+                yeniDersView.spnYeniDersKredi.setSelection(spinnerDegerinIndexiniBul(spnDersKredi, dersKredi))
+                yeniDersView.spnYeniDersNot.setSelection(spinnerDegerinIndexiniBul(spnDersNot, dersHarf))
+
+
+                yeniDersView.btnDersSil.setOnClickListener {
+
+                    rootLayout.removeView(yeniDersView)
+                    if(rootLayout.childCount == 0){
+                        btnOrtalamaHesapla.visibility = View.INVISIBLE
+                    }else btnOrtalamaHesapla.visibility = View.VISIBLE
+                }
 
 
 
-            rootLayout.addView(yeniDersView)
+                rootLayout.addView(yeniDersView)
+
+                btnOrtalamaHesapla.visibility = View.VISIBLE
+
+                sifirla()
+
+            }else {
+                Toast.makeText(this, "Ders Adını Giriniz", Toast.LENGTH_LONG).show()
+            }
+
+
 
 
         }
 
     }
 
+
+    fun sifirla(){
+        etDersAd.setText("")
+        spnDersKredi.setSelection(0)
+        spnDersNot.setSelection(0)
+    }
 
 
 
